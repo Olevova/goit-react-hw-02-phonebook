a532de0960dbe247c886c2bb413ff3aeb979953f
Olevova <olevova1983@gmail.com>
2022-12-14 22:19:55 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from "react";
+import { Contact } from "contact/contact";
+
+export class App extends React.Component {
+  
+state = {
+  contacts: [{name:"tolik", number:155}],
+  name: '',
+  number: ''
+}
+  handleName = ev =>
+  {
+    this.setState({ [ev.currentTarget.name]: ev.currentTarget.value});
+  }
+
+  handleAdd = (e) => {
+    console.log(e);
+    this.setState(({contacts}) => ({
+      contacts: [...contacts, {name: this.state.name, number: this.state.number } ]
+    }
+    ))
+
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Contact
+          name={this.state.name}
+          number={this.state.number}
+          func={this.handleName}
+          func2={this.handleAdd } />
+      </div>
+    );
+  }
 };
